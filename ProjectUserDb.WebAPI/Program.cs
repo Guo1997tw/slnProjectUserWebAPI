2deeee651d6ffd9963f11e95835d357cd0cd2cfd
h1162 <h1162@111C796>
2023-05-17 09:56:00 +0800

    2023/05/17 Update

--- ProjectUserDb.WebAPI/Program.cs
+++ ProjectUserDb.WebAPI/Program.cs
@@ -1,45 +1,58 @@
 using CoreProfiler.Web;
+using Evertrust.Core.Logging.Abstractions;
+using Evertrust.Core.Logging.Exceptionless;
+using Evertrust.ResponseWrapper.Extensions;
+using Evertrust.ResponseWrapper.Middlewares;
+using Exceptionless;
+using Microsoft.AspNetCore.Mvc;
 using ProjectUser.Repository.Helpers;
 using ProjectUser.Repository.Interface;
 using ProjectUser.Repository.Repository;
 using ProjectUser.Services.Interface;
 using ProjectUser.Services.Mapping;
 using ProjectUser.Services.Services;
-using ProjectUser.WebAPI.Filter;
+using ProjectUser.WebAPI;
 using ProjectUser.WebAPI.Infrastructure.Mapping;
 using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Add DI
+//Add Dependency Injection
 builder.Services.AddScoped<IDatabaseHelper, DatabaseHelper>();
-//builder.Services.AddScoped<IDatabaseHelper>(Option => new DatabaseHelper("UserDbContext"));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
-// Add services to the container.
-builder.Services.AddControllers();
+//Add Evertrust ResponseWrapper
+builder.Services.AddEvertrustResponseWrapper();
 
+builder.Services.AddControllers(x =>
+{
+    x.AddEvertrustResponseWrapperFilters();
+});
+
+builder.Services.AddApiVersioning(x =>
+{
+    x.ReportApiVersions = true;
+    x.AssumeDefaultVersionWhenUnspecified = true;
+    x.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+builder.Services.AddSingleton<ILogHelperFactory, LogHelperFactory>();
+
+//AutoMapper
 builder.Services.AddAutoMapper( x =>
 {
     x.AddProfile<ServiceProfile>();
     x.AddProfile<WebApplicationProfile>();
 });
 
-builder.Services.AddControllers(options =>
-{
-    // 在這裡設定全域過濾器
-    options.Filters.Add(typeof(RequestExceptionFilter));
-});
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 //Customized Swagger
-builder.Services.AddSwaggerGen(options =>
+builder.Services.AddSwaggerGen(x =>
 {
-    options.SwaggerDoc
+    x.SwaggerDoc
     (
         "v1",
         new()
@@ -50,20 +63,28 @@ builder.Services.AddSwaggerGen(options =>
         }
     );
 
-    options.ExampleFilters();
+    x.ExampleFilters();
 
     //var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     //options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-})
-    .AddSwaggerExamplesFromAssemblyOf<Program>();
+}).AddSwaggerExamplesFromAssemblyOf<Program>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+    app.UseSwagger().UseSwaggerUI(x =>
+    {
+        x.SwaggerEndpoint
+        (
+            url: "/swagger/v1/swagger.json",
+            name: "slnProjectUserWebAPI"
+        );
+    });
 }
 
 //CoreProfiler
@@ -71,7 +92,16 @@ app.UseCoreProfiler(true);
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+//Add Evertrust ResponseWrapper
+app.UseEvertrustResponseWrapper(typeof(Startup).Assembly);
+
+app.UseEvertrustExceptionHandling();
+
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 app.MapControllers();
 
